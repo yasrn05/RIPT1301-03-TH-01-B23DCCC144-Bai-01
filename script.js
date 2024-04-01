296206ab579751a1dffac3fde5d47e5c4cdf981f
yasrn <sangtd2005@gmail.com>
2024-04-01 09:08:41 +0700

    clock

---
+++ script.js
@@ -0,0 +1,15 @@
+function updateClock() {
+    var now = new Date();
+    var hours = now.getHours();
+    var minutes = now.getMinutes();
+    var seconds = now.getSeconds();
+
+    hours = hours < 10 ? '0' + hours : hours;
+    minutes = minutes < 10 ? '0' + minutes : minutes;
+    seconds = seconds < 10 ? '0' + seconds : seconds;
+
+    var clockDisplay = hours + ':' + minutes + ':' + seconds;
+    document.getElementById('clock').innerText = clockDisplay;
+}
+
+setInterval(updateClock, 1000);
